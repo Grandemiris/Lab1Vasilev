ab7cc9e79e4c62bbb02f38628c6c2b235b14e394
Grandemiris <77978885+Grandemiris@users.noreply.github.com>
2022-03-22 13:15:37 +0300

    Add files via upload

---
+++ Lab1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1 - Инкапсуляция");
+            Console.WriteLine("Выполнил - Заинчковский\n");
+            Console.WriteLine("Задание 1:");
+
+            Complex a = new Complex() { Real = 3, Imag = 0.5 };
+            Complex b = new Complex() { Real = 2, Imag = 0.8 };
+
+            a.Add(b);
+            b.Substract(a);
+
+
+            Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
+            Console.WriteLine("b = {0} + {1}i", b.Real, b.Imag);
+
+            a.Multiply(b);
+            Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
+
+            b.Divide(a);
+            Console.WriteLine("a = {0} + {1}i", b.Real, b.Imag);
+
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Задание 2:");
+
+            Student s1 = new Student() { name = "Василий", gender = "мужчина", hairColor = "Желтый", age = 20 };
+            Student s2 = new Student() { name = "Маша", gender = "женщина", hairColor = "Черный", age = 21 };
+
+            s1.Add(s2);
+            Console.WriteLine("Имя = {0} | Пол = {1} | Цвет волос = {2} | Возраст = {3}", s1.name, s1.gender, s1.hairColor, s1.age);
+
+        }
+    }
+}
---
+++ Lab1/Zadanie2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Student
+    {
+        public string name { get; set; }
+        public string gender { get; set; }
+        public string hairColor { get; set; }
+        public int age { get; set; }
+
+        public void Add(Student x)
+        {
+            name += x.name;
+            gender += x.gender;
+            hairColor += x.hairColor;
+            age += x.age;
+        }
+
+
+    }
+}
